89a1db99601056d7d85adfea157742189dd1f6da
hong-t <th13564039482@163.com>
2021-10-17 17:00:43 +0000

    fix rabbitmq register

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CONSUL_HTTP_ADDR=${ENV_CONSUL_HOST}:${ENV_CONSUL_PORT} consul services register -address=rabbitmq.${ENV_CLUSTER_NAMESPACE}.svc.cluster.local -name=rabbitmq.npool.top -port=15672
+CONSUL_HTTP_ADDR=${ENV_CONSUL_HOST}:${ENV_CONSUL_PORT} consul services register -address=rabbitmq.${ENV_CLUSTER_NAMESPACE}.svc.cluster.local -name=rabbitmq.npool.top -port=5672
 if [ ! $? -eq 0 ]; then
   echo "FAIL TO REGISTER ME TO CONSUL"
   exit 1
